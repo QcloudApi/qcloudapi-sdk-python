55a5fca9b362315706326a7d24f616d5867e4d12
QcloudApi <QcloudApi@users.noreply.github.com>
2015-07-28 10:06:54 +0800

    Update qcloudapi.py

--- src/QcloudApi/qcloudapi.py
+++ src/QcloudApi/qcloudapi.py
@@ -34,10 +34,10 @@ class QcloudApi:
         elif (module == 'vod'):
             from modules.vod import Vod
             service = Vod(config)
-		elif (module == 'yunsou'):
+	elif (module == 'yunsou'):
             from modules.yunsou import Yunsou
             service = Yunsou(config)
-		elif (module == 'wenzhi'):
+	elif (module == 'wenzhi'):
             from modules.wenzhi import Wenzhi
             service = Wenzhi(config)
         else:
