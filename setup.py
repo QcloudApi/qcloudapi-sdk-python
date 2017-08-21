df0e4f58ca92bca1b88f1f133a9fb76cb660bb45
jimmyzhuang <jimmyzhuang@tencent.com>
2017-08-21 16:36:38 +0800

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ ROOT = os.path.dirname(__file__)
 
 setup(
     name='qcloudapi-sdk-python',
-    version='2.0.3',
+    version='2.0.4',
     description='The Qcloud Api SDK for Python',
     long_description=open('README.rst').read(),
     author='Qcloud',
